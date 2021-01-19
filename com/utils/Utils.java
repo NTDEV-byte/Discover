19191760b2a7de9da6ccd9471f0a9586661f1f8d
bookman <nassim.t998@gmail.com>
2021-01-19 22:55:19 +0100

    added utils class and static method windowLauncher to create directly a window without retyping it everytime

---
+++ com/utils/Utils.java
@@ -0,0 +1,38 @@
+package com.utils;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Utils {
+		
+		private Utils() {
+			
+		
+		}
+
+		
+		
+		public static void windowLauncher(String title,JPanel panel) { 
+			 	JFrame window = new JFrame(title);
+			 	window.setPreferredSize(new Dimension(800,600));
+			 	window.setLocationRelativeTo(null);
+			 	window.setVisible(true);
+			 	window.setResizable(false);
+			 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			 	window.add(panel);
+			 	window.pack();
+		}
+		
+		public static void windowLauncher(String title,int width,int height,JPanel panel) { 
+		 	JFrame window = new JFrame(title);
+		 	window.setPreferredSize(new Dimension(width,height));
+		 	window.setLocationRelativeTo(null);
+		 	window.setVisible(true);
+		 	window.setResizable(false);
+		 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 	window.add(panel);
+		 	window.pack();
+	}
+}
